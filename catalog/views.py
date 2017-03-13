e8ff4ec56204f85f4a7b8862bd94d9035a301513
Volodymyr Pavlenko <pavlenkov1996@gmail.com>
2017-03-13 14:31:26 +0200

    Сreate the detail view template

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views import generic
 from .models import Author, Book, BookInstance, Genre
 
 
@@ -24,3 +24,12 @@ def index(request):
                  num_instances_available, 'num_authors': num_authors, 'num_genres': num_genres,
                  'num_word': num_word},
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
