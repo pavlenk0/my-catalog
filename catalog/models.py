ed2b8d91e77b8cc4a665f075e14dd34c3e1f0813
Volodymyr Pavlenko <pavlenkov1996@gmail.com>
2017-03-16 12:52:49 +0200

    Create book form to create, edit and delete books

--- catalog/models.py
+++ catalog/models.py
@@ -46,6 +46,7 @@ class Book(models.Model):
     isbn = models.CharField('ISBN', max_length=13, help_text='''13 Character
                             <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>''')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
 
